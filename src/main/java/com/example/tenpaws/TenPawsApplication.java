872c5fcb9c67eb203e663d7bf3ccbb5a4807b41e
wnsgho <darkblueuu@gmail.com>
2024-11-18 10:04:12 +0900

    start

---
+++ src/main/java/com/example/tenpaws/TenPawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tenpaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenPawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenPawsApplication.class, args);
+    }
+
+}
