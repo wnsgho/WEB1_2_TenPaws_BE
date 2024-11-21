14f8b085c90487308a7e230953333829d679c088
namhyeonh0 <www101365@naver.com>
2024-11-21 09:12:58 +0900

    feat : ChatRoomRequest DTO 추가

---
+++ src/main/java/com/example/tenpaws/domain/chat/chatroom/dto/ChatRoomRequest.java
@@ -0,0 +1,30 @@
+package com.example.tenpaws.domain.chat.chatroom.dto;
+
+import com.example.tenpaws.domain.chat.chatroom.entity.ChatRoom;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ChatRoomRequest {
+    @NotBlank
+    private Long userId;
+
+    @NotBlank
+    private Long shelterId;
+
+    public ChatRoomRequest(ChatRoom chatRoom) {
+        this.userId = chatRoom.getUserId();
+        this.shelterId = chatRoom.getShelterId();
+    }
+
+    public ChatRoom toEntity() {
+        return ChatRoom.builder()
+                .userId(userId)
+                .shelterId(shelterId)
+                .build();
+    }
+}
