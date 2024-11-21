0afdd4cae54df60848abbb019c3b6bf6195a5044
kimdongjoon-web <color6763@naver.com>
2024-11-21 21:47:46 +0900

    feat : 공지사항 작성자, 작성일 기능 추가

--- src/main/java/com/example/tenpaws/domain/board/entity/Announcement.java
+++ src/main/java/com/example/tenpaws/domain/board/entity/Announcement.java
@@ -1,14 +1,20 @@
 package com.example.tenpaws.domain.board.entity;
 
+import com.example.tenpaws.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "Announcements")
 public class Announcement {
 
@@ -17,14 +23,22 @@ public class Announcement {
     @Column(name = "id", updatable = false)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     @Column(name = "title", nullable = false)
     private String title;
 
     @Column(name = "content", nullable = false)
     private String content;
 
+    @CreatedDate
+    private LocalDate created_at;
+
     @Builder
-    public Announcement(String title, String content) {
+    public Announcement(User user, String title, String content) {
+        this.user = user;
         this.title = title;
         this.content = content;
     }
