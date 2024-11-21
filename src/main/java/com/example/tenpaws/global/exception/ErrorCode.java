b4707ce8c17d9a6891c983b2a10a242ee37cb842
namhyeonh0 <www101365@naver.com>
2024-11-21 09:45:44 +0900

    add : ErrorCode ChatRoom 관련 에러 코드 추가

--- src/main/java/com/example/tenpaws/global/exception/ErrorCode.java
+++ src/main/java/com/example/tenpaws/global/exception/ErrorCode.java
@@ -22,6 +22,12 @@ public enum ErrorCode {
     FAQ_NOT_DELETE(HttpStatus.BAD_REQUEST, "Faq not deleted"),
     FAQ_NO_CHILD(HttpStatus.BAD_REQUEST, "This Faq has no children"),
 
+    // ChatRoom
+    ChatRoom_NOT_FOUND(HttpStatus.NOT_FOUND, "ChatRoom not found"),
+    ChatRoom_NOT_REGISTERED(HttpStatus.BAD_REQUEST, "ChatRoom not registered"),
+    ChatRoom_NOT_MODIFIED(HttpStatus.BAD_REQUEST, "ChatRoom not modified"),
+    ChatRoom_NOT_DELETE(HttpStatus.BAD_REQUEST, "ChatRoom not deleted"),
+
     // Announcement
     ANNOUNCEMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Announcement not found");
 
