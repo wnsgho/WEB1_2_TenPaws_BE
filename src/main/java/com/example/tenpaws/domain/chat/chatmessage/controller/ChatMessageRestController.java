f7d67669ba213d25f0e62f9b8cc9ab136a75539e
namhyeonh0 <www101365@naver.com>
2024-11-21 14:42:15 +0900

    mod : ChatMessageRestController 일반 컨트롤러와 구분을 위한 Rest 명칭 추가

--- src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatMessageRestController.java
+++ src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatMessageRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/chatmessages")
-public class ChatMessageController {
+public class ChatMessageRestController {
     private final ChatMessageService chatMessageService;
 
     @GetMapping("/{chatRoomId}")
