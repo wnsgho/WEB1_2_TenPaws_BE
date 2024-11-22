47863b2ca1d08fe1871e3a00ee5a2a41cc6c55e5
wnsgho <darkblueuu@gmail.com>
2024-11-22 10:08:15 +0900

    Shelter, Pet CRUD, Controller

--- src/main/java/com/example/tenpaws/domain/shelter/entity/Shelter.java
+++ src/main/java/com/example/tenpaws/domain/shelter/entity/Shelter.java
@@ -1,7 +1,6 @@
 package com.example.tenpaws.domain.shelter.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
