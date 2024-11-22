bc99d29419273a9434734e6e2a3b77521eecdb4f
wnsgho <darkblueuu@gmail.com>
2024-11-22 17:36:52 +0900

    Update TenPawsApplication.java

--- src/main/java/com/example/tenpaws/TenPawsApplication.java
+++ src/main/java/com/example/tenpaws/TenPawsApplication.java
@@ -3,7 +3,7 @@ package com.example.tenpaws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
