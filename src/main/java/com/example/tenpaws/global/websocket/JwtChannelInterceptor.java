9cf672c8f800637c2a2c0676aa51d9a7f4649b78
namhyeonh0 <www101365@naver.com>
2024-11-26 15:52:11 +0900

    mod : JwtChannelInterceptor JWT 검증 로직 수정

--- src/main/java/com/example/tenpaws/global/websocket/JwtChannelInterceptor.java
+++ src/main/java/com/example/tenpaws/global/websocket/JwtChannelInterceptor.java
@@ -1,4 +1,4 @@
-package com.example.tenpaws.global.interceptor;
+package com.example.tenpaws.global.websocket;
 
 import com.example.tenpaws.global.security.jwt.JwtUtil;
 import io.jsonwebtoken.JwtException;
@@ -9,8 +9,6 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -29,9 +27,10 @@ public class JwtChannelInterceptor implements ChannelInterceptor {
             if (authorization != null && !authorization.isEmpty()) {
                 String token = authorization.get(0).replace("Bearer ", "");
                 try {
-                    String email = jwtUtil.getEmail(token);
-                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(email, null, null);
-                    SecurityContextHolder.getContext().setAuthentication(auth);
+                    if (jwtUtil.isExpired(token)) {
+                        log.warn("JWT token expired");
+                        return null;
+                    }
                 } catch (JwtException e) {
                     log.error("Invalid JWT: {}", e.getMessage());
                     return null;
