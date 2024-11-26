f1eb643f0bde473638c0e692698c7750fd159c19
xxxkyeom <seonggyeomgim97@gmail.com>
2024-11-26 11:21:11 +0900

    add: 사용자 이름을 위한 username 필드 추가 및 메소드 수정

--- src/main/java/com/example/tenpaws/domain/user/dto/UserJoinDTO.java
+++ src/main/java/com/example/tenpaws/domain/user/dto/UserJoinDTO.java
@@ -23,6 +23,9 @@ public class UserJoinDTO {
     )
     private String email;
 
+    @NotBlank(message = "사용자 이름은 필수입니다")
+    private String username;
+
     @NotBlank(message = "사용자 비밀번호는 필수입니다")
     @Size(min = 7, max = 20, message = "사용자 비밀번호는 7글자 이상, 20글자 이하로 제한됩니다")
     private String password;
@@ -47,6 +50,7 @@ public class UserJoinDTO {
     public User toEntity() {
         return User.builder()
                 .email(this.email)
+                .username(this.username)
                 .password(this.password)
                 .birthDate(this.birthDate)
                 .phoneNumber(this.phoneNumber)
