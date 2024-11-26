3fae3b1dc76605257d61ee88364fbc083b0952bd
namhyeonh0 <www101365@naver.com>
2024-11-26 15:54:37 +0900

    add : 채팅 관련 알림 추가

--- src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatController.java
+++ src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatController.java
@@ -1,11 +1,14 @@
 package com.example.tenpaws.domain.chat.chatmessage.controller;
 
 import com.example.tenpaws.domain.chat.chatmessage.dto.ChatMessageRequest;
+import com.example.tenpaws.domain.chat.chatmessage.dto.ChatMessageResponse;
 import com.example.tenpaws.domain.chat.chatmessage.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,14 +16,38 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
     private final ChatMessageService chatMessageService;
     private final SimpMessagingTemplate messagingTemplate;
+    private final SimpUserRegistry userRegistry;
 
     @MessageMapping("/chat/send/{chatRoomId}")
     public void sendMessage(@DestinationVariable String chatRoomId, ChatMessageRequest chatMessageRequest) {
+        printUserSubscriptions(userRegistry);
+        String receiver = chatMessageRequest.getReceiver();
+        boolean isReceiverConnected = userRegistry.getUsers().stream()
+                .anyMatch(user -> user.getName().equals(receiver));
+
         chatMessageRequest.setChatRoomId(Long.valueOf(chatRoomId));
-        chatMessageService.createChatMessage(chatMessageRequest);
+        ChatMessageResponse chatMessageResponse = chatMessageService.createChatMessage(chatMessageRequest);
         messagingTemplate.convertAndSend(
                 "/topic/chatroom/" + chatRoomId,
-                "{\"content\":\"" + chatMessageRequest.getMessage() + "\", \"username\":\"" + chatMessageRequest.getSender() + "\"}"
+                chatMessageResponse
         );
+        if (!isReceiverConnected) {
+            System.out.println("상대방 접속 X");
+            messagingTemplate.convertAndSendToUser(
+                    receiver,
+                    "/queue/notifications",
+                    "새 채팅 메시지가 도착했습니다!"
+            );
+        }
+    }
+
+    public void printUserSubscriptions(SimpUserRegistry userRegistry) {
+        System.out.println("userRegistry: " + userRegistry);
+        for (SimpUser user : userRegistry.getUsers()) {
+            System.out.println("User: " + user.getName());
+            user.getSessions().forEach(session -> {
+                System.out.println("Subscribed to: " + session.getSubscriptions());
+            });
+        }
     }
 }
