32cc44ba7fc665032029f028c172575f60bf767d
xxxkyeom <seonggyeomgim97@gmail.com>
2024-11-28 23:04:40 +0900

    add: 관리자의 인원 체크를 위한 메소드 추가

--- src/main/java/com/example/tenpaws/domain/admin/repository/AdminRepository.java
+++ src/main/java/com/example/tenpaws/domain/admin/repository/AdminRepository.java
@@ -1,7 +1,9 @@
 package com.example.tenpaws.domain.admin.repository;
 
 import com.example.tenpaws.domain.admin.entity.Admin;
+import com.example.tenpaws.global.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    int countByUserRole(UserRole userRole);
 }
