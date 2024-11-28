094e8c1a7befd5795e6fd8ea45c75b8e6c2ebd62
namhyeonh0 <www101365@naver.com>
2024-11-28 12:08:31 +0900

    add : UnReadChatMessagesRequest 생성자 추가

--- src/main/java/com/example/tenpaws/domain/chat/unread/dto/UnReadChatMessagesRequest.java
+++ src/main/java/com/example/tenpaws/domain/chat/unread/dto/UnReadChatMessagesRequest.java
@@ -2,6 +2,7 @@ package com.example.tenpaws.domain.chat.unread.dto;
 
 import com.example.tenpaws.domain.chat.unread.entity.UnReadChatMessages;
 import com.example.tenpaws.domain.chat.chatroom.entity.ChatRoom;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -14,6 +15,12 @@ public class UnReadChatMessagesRequest {
 
     @NonNull
     private String username;
+
+    @Builder
+    public UnReadChatMessagesRequest(@NonNull Long chatRoomId, @NonNull String username) {
+        this.chatRoomId = chatRoomId;
+        this.username = username;
+    }
 
     public UnReadChatMessages toEntity(ChatRoom chatRoom) {
         return UnReadChatMessages.builder()
