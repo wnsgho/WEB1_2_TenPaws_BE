cda3f243dfb0b867f53be2d10a23d67340054634
namhyeonh0 <www101365@naver.com>
2024-11-28 12:09:26 +0900

    mod : UnReadChatMessagesRepository findByChatRoomIdAndUsername 메서드 반환 타입 변경

--- src/main/java/com/example/tenpaws/domain/chat/unread/repository/UnReadChatMessagesRepository.java
+++ src/main/java/com/example/tenpaws/domain/chat/unread/repository/UnReadChatMessagesRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UnReadChatMessagesRepository extends JpaRepository<UnReadChatMessages, Long> {
     @Query("select uc from UnReadChatMessages uc where uc.username = :username")
     List<UnReadChatMessages> findBydUsername(String username);
 
     @Query("select uc from UnReadChatMessages uc where uc.chatroom.id = :chatRoomId and uc.username = :username")
-    UnReadChatMessages findByChatRoomIdAndUsername(Long chatRoomId, String username);
+    Optional<UnReadChatMessages> findByChatRoomIdAndUsername(Long chatRoomId, String username);
 }
