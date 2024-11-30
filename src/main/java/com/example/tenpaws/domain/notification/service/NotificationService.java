d6a0c108d4011eae6a90cc086f66e65270195c01
kimdongjoon-web <color6763@naver.com>
2024-11-30 23:51:18 +0900

    feat : 사용자 식별 기능 추가

--- src/main/java/com/example/tenpaws/domain/notification/service/NotificationService.java
+++ src/main/java/com/example/tenpaws/domain/notification/service/NotificationService.java
@@ -3,13 +3,14 @@ package com.example.tenpaws.domain.notification.service;
 import com.example.tenpaws.domain.notification.dto.request.CreateNotificationRequest;
 import com.example.tenpaws.domain.notification.dto.response.NotificationResponse;
 import com.example.tenpaws.domain.notification.entity.Notification;
+import com.example.tenpaws.global.entity.UserRole;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public interface NotificationService {
-    SseEmitter subscribe(Long userId);
-    void notify(Long userId, NotificationResponse notification);
+    SseEmitter subscribe(UserRole userRole, Long userId);
+    void notify(UserRole userRole, Long userId, NotificationResponse notification);
 
     Notification create(CreateNotificationRequest request);
     Page<NotificationResponse> getList(Long userId, Pageable pageable);
