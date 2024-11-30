057b83d822c517121452f667a5dade4a90af52eb
kimdongjoon-web <color6763@naver.com>
2024-11-30 23:59:28 +0900

    chore : 코드 수정

--- src/main/java/com/example/tenpaws/domain/board/repository/CommentRepository.java
+++ src/main/java/com/example/tenpaws/domain/board/repository/CommentRepository.java
@@ -3,8 +3,5 @@ package com.example.tenpaws.domain.board.repository;
 import com.example.tenpaws.domain.board.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByInquiryId(Long inquiryId);
 }
