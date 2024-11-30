b2d1b16a49f0e29ae43de2f25eb982f7c6ec9932
kimdongjoon-web <color6763@naver.com>
2024-11-30 18:47:13 +0900

    feat : 문의 목록 응답 DTO 추가

---
+++ src/main/java/com/example/tenpaws/domain/board/dto/response/InquiryListViewResponse.java
@@ -0,0 +1,23 @@
+package com.example.tenpaws.domain.board.dto.response;
+
+import com.example.tenpaws.domain.board.entity.Inquiry;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class InquiryListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String userName;
+    private final Long viewCount;
+    private final LocalDate created_at;
+
+    public InquiryListViewResponse(Inquiry inquiry) {
+        this.id = inquiry.getId();
+        this.title = inquiry.getTitle();
+        this.userName = inquiry.getUser().getUsername();
+        this.viewCount = inquiry.getViewCount();
+        this.created_at = inquiry.getCreated_at();
+    }
+}
