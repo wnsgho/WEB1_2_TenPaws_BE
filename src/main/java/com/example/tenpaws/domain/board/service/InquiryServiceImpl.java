86bf3040f10662d18f24af2bfcd9306619d7f7ca
kimdongjoon-web <color6763@naver.com>
2024-12-01 14:05:01 +0900

    chore : import 제거

--- src/main/java/com/example/tenpaws/domain/board/service/InquiryServiceImpl.java
+++ src/main/java/com/example/tenpaws/domain/board/service/InquiryServiceImpl.java
@@ -19,7 +19,6 @@ import com.example.tenpaws.global.utils.AuthenticationUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
