518a5f21d44e09c35ceda6a2a3a60d2dbefe2c02
kimdongjoon-web <color6763@naver.com>
2024-12-01 14:04:07 +0900

    feat : EnableMethodSecurity 메소드 검증 기능 추가

--- src/main/java/com/example/tenpaws/global/config/SecurityConfig.java
+++ src/main/java/com/example/tenpaws/global/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,6 +29,7 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
