b1b4201b5574350a1763fc45817ee9586bb2e53c
xxxkyeom <seonggyeomgim97@gmail.com>
2024-12-02 17:06:54 +0900

    add: 메서드 추가

--- src/main/java/com/example/tenpaws/domain/admin/repository/AdminRepository.java
+++ src/main/java/com/example/tenpaws/domain/admin/repository/AdminRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     int countByUserRole(UserRole userRole);
     Optional<Admin> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
