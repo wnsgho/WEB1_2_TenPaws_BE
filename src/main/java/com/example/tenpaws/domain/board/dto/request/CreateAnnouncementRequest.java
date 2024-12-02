ac11f3adc12b90e2dba1c4183330ce7df5157167
kimdongjoon-web <color6763@naver.com>
2024-12-02 13:13:16 +0900

    feat : 공지사항 생성 시 카테고리, 조회 수 항목 추가

--- src/main/java/com/example/tenpaws/domain/board/dto/request/CreateAnnouncementRequest.java
+++ src/main/java/com/example/tenpaws/domain/board/dto/request/CreateAnnouncementRequest.java
@@ -2,6 +2,7 @@ package com.example.tenpaws.domain.board.dto.request;
 
 import com.example.tenpaws.domain.admin.entity.Admin;
 import com.example.tenpaws.domain.board.entity.Announcement;
+import com.example.tenpaws.domain.board.entity.AnnouncementCategory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +13,17 @@ import lombok.NoArgsConstructor;
 public class CreateAnnouncementRequest {
 
     private Long adminId;
+    private AnnouncementCategory category;
     private String title;
     private String content;
 
     public Announcement toEntity(Admin admin) {
         return Announcement.builder()
                 .admin(admin)
+                .category(category)
                 .title(title)
                 .content(content)
+                .viewCount(0L)
                 .build();
     }
 }
