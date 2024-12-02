d20bc127fe12c4ab3909f9bb509880d7917a0a7f
namhyeonh0 <www101365@naver.com>
2024-12-02 16:11:09 +0900

    add : ChatMessageRestController ChatMessageResponse의 senderName 필드 추가에 따른 코드 추가

--- src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatMessageRestController.java
+++ src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatMessageRestController.java
@@ -2,6 +2,7 @@ package com.example.tenpaws.domain.chat.chatmessage.controller;
 
 import com.example.tenpaws.domain.chat.chatmessage.dto.ChatMessageResponse;
 import com.example.tenpaws.domain.chat.chatmessage.service.ChatMessageService;
+import com.example.tenpaws.global.security.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/chatmessages")
 public class ChatMessageRestController {
     private final ChatMessageService chatMessageService;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @GetMapping("/{chatRoomId}")
     public ResponseEntity<List<ChatMessageResponse>> getChatMessages(@PathVariable("chatRoomId") Long chatRoomId) {
-        return ResponseEntity.ok(chatMessageService.getChatMessagesByChatRoomId(chatRoomId));
+        List<ChatMessageResponse> chatMessageResponseList = chatMessageService.getChatMessagesByChatRoomId(chatRoomId);
+        List<ChatMessageResponse> list = chatMessageResponseList.stream().peek(chatMessageResponse -> {
+            Map<String, Object> userDetails = customUserDetailsService.getInfosByEmail(chatMessageResponse.getSenderEmail());
+            chatMessageResponse.setSenderName(userDetails.get("username").toString());
+        }).toList();
+        return ResponseEntity.ok(list);
     }
 }
