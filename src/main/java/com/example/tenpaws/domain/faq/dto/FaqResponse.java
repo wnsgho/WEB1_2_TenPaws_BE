b37cdda95cee749d801db0ffabcc46d97170d709
namhyeonh0 <www101365@naver.com>
2024-12-02 14:52:14 +0900

    mod : FaqResponse 수정

--- src/main/java/com/example/tenpaws/domain/faq/dto/FaqResponse.java
+++ src/main/java/com/example/tenpaws/domain/faq/dto/FaqResponse.java
@@ -2,8 +2,10 @@ package com.example.tenpaws.domain.faq.dto;
 
 import com.example.tenpaws.domain.faq.entity.Faq;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 public class FaqResponse {
     @NotBlank
@@ -12,11 +14,11 @@ public class FaqResponse {
     @NotBlank
     private String content;
 
-    private Long refFaqId;
+    private Long parentId;
 
     public FaqResponse(Faq faq) {
         this.faqId = faq.getId();
         this.content = faq.getContent();
-        this.refFaqId = faq.getParent() == null ? null : faq.getParent().getId();
+        this.parentId = faq.getParent() == null ? null : faq.getParent().getId();
     }
 }
