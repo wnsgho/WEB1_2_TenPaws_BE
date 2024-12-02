746907661edd434fb54a7bd0b563024baebf7681
xxxkyeom <seonggyeomgim97@gmail.com>
2024-12-02 12:30:29 +0900

    add: 메서드 추가

--- src/main/java/com/example/tenpaws/domain/admin/repository/AdminRepository.java
+++ src/main/java/com/example/tenpaws/domain/admin/repository/AdminRepository.java
@@ -4,6 +4,9 @@ import com.example.tenpaws.domain.admin.entity.Admin;
 import com.example.tenpaws.global.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     int countByUserRole(UserRole userRole);
+    Optional<Admin> findByEmail(String email);
 }
