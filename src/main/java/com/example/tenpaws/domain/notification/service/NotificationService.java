13996d7e69bc1193a21118ffa2a107a4adb45d79
kimdongjoon-web <color6763@naver.com>
2024-12-02 20:00:09 +0900

    chore : import 제거

--- src/main/java/com/example/tenpaws/domain/notification/service/NotificationService.java
+++ src/main/java/com/example/tenpaws/domain/notification/service/NotificationService.java
@@ -2,7 +2,6 @@ package com.example.tenpaws.domain.notification.service;
 
 import com.example.tenpaws.domain.notification.dto.request.CreateNotificationRequest;
 import com.example.tenpaws.domain.notification.dto.response.NotificationResponse;
-import com.example.tenpaws.domain.notification.entity.Notification;
 import com.example.tenpaws.global.entity.UserRole;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
