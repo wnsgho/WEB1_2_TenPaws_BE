ab4a326979a2437999d11f34eac8e3a3ac6533ea
kimdongjoon-web <color6763@naver.com>
2024-12-03 09:47:28 +0900

    refactor : DTO를 반환하도록 수정

--- src/main/java/com/example/tenpaws/domain/board/service/AnnouncementService.java
+++ src/main/java/com/example/tenpaws/domain/board/service/AnnouncementService.java
@@ -4,14 +4,13 @@ import com.example.tenpaws.domain.board.dto.request.CreateAnnouncementRequest;
 import com.example.tenpaws.domain.board.dto.request.UpdateAnnouncementRequest;
 import com.example.tenpaws.domain.board.dto.response.AnnouncementListViewResponse;
 import com.example.tenpaws.domain.board.dto.response.AnnouncementResponse;
-import com.example.tenpaws.domain.board.entity.Announcement;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AnnouncementService {
-    Announcement create(CreateAnnouncementRequest request);
+    AnnouncementResponse create(CreateAnnouncementRequest request);
     Page<AnnouncementListViewResponse> getList(Pageable pageable);
     AnnouncementResponse findById(Long announcementId);
-    Announcement update(Long announcementId, UpdateAnnouncementRequest request);
+    AnnouncementResponse update(Long announcementId, UpdateAnnouncementRequest request);
     void delete(Long announcementId);
 }
