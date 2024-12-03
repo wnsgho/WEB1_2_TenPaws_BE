ebb365be65d7689bd9eb946641a85ac1d4e43964
kimdongjoon-web <color6763@naver.com>
2024-12-03 09:48:51 +0900

    refactor : DTO를 반환하도록 수정

--- src/main/java/com/example/tenpaws/domain/board/service/InquiryService.java
+++ src/main/java/com/example/tenpaws/domain/board/service/InquiryService.java
@@ -4,14 +4,14 @@ import com.example.tenpaws.domain.board.dto.request.CreateInquiryRequest;
 import com.example.tenpaws.domain.board.dto.request.UpdateInquiryRequest;
 import com.example.tenpaws.domain.board.dto.response.InquiryDetailResponse;
 import com.example.tenpaws.domain.board.dto.response.InquiryListViewResponse;
-import com.example.tenpaws.domain.board.entity.Inquiry;
+import com.example.tenpaws.domain.board.dto.response.InquiryResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface InquiryService {
-    Inquiry create(CreateInquiryRequest request);
+    InquiryResponse create(CreateInquiryRequest request);
     Page<InquiryListViewResponse> getList(Pageable pageable);
     InquiryDetailResponse findById(Long inquiryId);
-    Inquiry update(Long inquiryId, UpdateInquiryRequest request);
-    void delete(Long inquiryId, Long userId, Long shelterId);
+    InquiryResponse update(Long inquiryId, UpdateInquiryRequest request);
+    void delete(Long inquiryId);
 }
