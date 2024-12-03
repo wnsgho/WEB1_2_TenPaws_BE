a1e1363e24c8255cf3cb6ca18490f0486feb2ddd
kimdongjoon-web <color6763@naver.com>
2024-12-03 09:47:55 +0900

    feat : 권한 확인, 본인 검증 추가

--- src/main/java/com/example/tenpaws/domain/board/controller/CommentController.java
+++ src/main/java/com/example/tenpaws/domain/board/controller/CommentController.java
@@ -3,7 +3,6 @@ package com.example.tenpaws.domain.board.controller;
 import com.example.tenpaws.domain.board.dto.request.CreateCommentRequest;
 import com.example.tenpaws.domain.board.dto.request.UpdateCommentRequest;
 import com.example.tenpaws.domain.board.dto.response.CommentResponse;
-import com.example.tenpaws.domain.board.entity.Comment;
 import com.example.tenpaws.domain.board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,28 +15,25 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService commentService;
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN')")
     @PostMapping
     public ResponseEntity<CommentResponse> create(
             @PathVariable Long inquiryId,
             @RequestBody CreateCommentRequest request) {
-        Comment comment = commentService.create(inquiryId, request);
-        return ResponseEntity.ok(new CommentResponse(comment));
+        return ResponseEntity.ok(commentService.create(inquiryId, request));
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN') and @ownershipVerifier.isCommentOwner(#commentId, authentication.name)")
     @PutMapping("/{commentId}")
     public ResponseEntity<CommentResponse> update(
             @PathVariable Long commentId,
             @RequestBody UpdateCommentRequest request) {
-        Comment comment = commentService.update(commentId, request);
-        return ResponseEntity.ok(new CommentResponse(comment));
+        return ResponseEntity.ok(commentService.update(commentId, request));
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN') and @ownershipVerifier.isCommentOwner(#commentId, authentication.name)")
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<String> delete(
-            @PathVariable Long commentId) {
+    public ResponseEntity<String> delete(@PathVariable Long commentId) {
         commentService.delete(commentId);
         return ResponseEntity.ok("답변이 성공적으로 삭제되었습니다.");
     }
