55e964e0ad690edebfa93df0cea4000f3c2f15cf
kimdongjoon-web <color6763@naver.com>
2024-12-03 12:19:51 +0900

    fix : dto 통합

--- src/main/java/com/example/tenpaws/domain/board/service/AnnouncementService.java
+++ src/main/java/com/example/tenpaws/domain/board/service/AnnouncementService.java
@@ -1,16 +1,15 @@
 package com.example.tenpaws.domain.board.service;
 
-import com.example.tenpaws.domain.board.dto.request.CreateAnnouncementRequest;
-import com.example.tenpaws.domain.board.dto.request.UpdateAnnouncementRequest;
+import com.example.tenpaws.domain.board.dto.request.AnnouncementRequest;
 import com.example.tenpaws.domain.board.dto.response.AnnouncementListViewResponse;
 import com.example.tenpaws.domain.board.dto.response.AnnouncementResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AnnouncementService {
-    AnnouncementResponse create(CreateAnnouncementRequest request);
+    AnnouncementResponse create(AnnouncementRequest request, String email);
     Page<AnnouncementListViewResponse> getList(Pageable pageable);
     AnnouncementResponse findById(Long announcementId);
-    AnnouncementResponse update(Long announcementId, UpdateAnnouncementRequest request);
+    AnnouncementResponse update(Long announcementId, AnnouncementRequest request);
     void delete(Long announcementId);
 }
