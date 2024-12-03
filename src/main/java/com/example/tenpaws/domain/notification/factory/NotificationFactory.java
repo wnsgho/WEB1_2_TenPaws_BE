bd26922692df191732e2890ec60f6802effe527d
kimdongjoon-web <color6763@naver.com>
2024-12-03 16:16:33 +0900

    mod : userId + userRole 대신 email로 검증

--- src/main/java/com/example/tenpaws/domain/notification/factory/NotificationFactory.java
+++ src/main/java/com/example/tenpaws/domain/notification/factory/NotificationFactory.java
@@ -1,6 +1,6 @@
 package com.example.tenpaws.domain.notification.factory;
 
-import com.example.tenpaws.domain.notification.dto.request.CreateNotificationRequest;
+import com.example.tenpaws.domain.notification.dto.request.NotificationRequest;
 import com.example.tenpaws.domain.notification.entity.NotificationType;
 import com.example.tenpaws.domain.notification.template.NotificationTemplate;
 import com.example.tenpaws.global.entity.UserRole;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NotificationFactory {
     private final NotificationTemplate template;
-    private static final Long INQUIRY_ADMIN_ID = 1L; // 문의 담당 관리자 고유번호
+    private static final String INQUIRY_ADMIN_EMAIL = "superadmin@tenpaws.com"; // 문의 담당 관리자 고유번호
 
 //    // 입양 신청 완료 알림 생성
 //    public CreateNotificationRequest createAdoptionSubmittedNotification(Long userId) {
@@ -41,22 +41,20 @@ public class NotificationFactory {
 //    }
 
     // 문의 게시판에 문의 등록 시 문의 담당 관리자에게 알림 전송
-    public CreateNotificationRequest createInquirySubmittedNotification(String email) {
-        return CreateNotificationRequest.builder()
+    public NotificationRequest createInquirySubmittedNotification(String senderEmail) {
+        return NotificationRequest.builder()
                 .type(NotificationType.INQUIRY_SUBMITTED)
-                .content(template.generateMessage(NotificationType.INQUIRY_SUBMITTED, email))
-                .userRole(UserRole.ROLE_ADMIN)
-                .userId(INQUIRY_ADMIN_ID)
+                .content(template.generateMessage(NotificationType.INQUIRY_SUBMITTED, senderEmail))
+                .recipientEmail(INQUIRY_ADMIN_EMAIL)
                 .build();
     }
 
     // 문의 답변 알림 생성
-    public CreateNotificationRequest createInquiryAnsweredNotification(Long userId, UserRole userRole) {
-        return CreateNotificationRequest.builder()
+    public NotificationRequest createInquiryAnsweredNotification(String recipientEmail) {
+        return NotificationRequest.builder()
                 .type(NotificationType.INQUIRY_ANSWERED)
                 .content(template.generateMessage(NotificationType.INQUIRY_ANSWERED))
-                .userRole(userRole)
-                .userId(userId)
+                .recipientEmail(recipientEmail)
                 .build();
     }
 }
