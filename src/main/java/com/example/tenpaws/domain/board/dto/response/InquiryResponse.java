09f24c77656a5b9d220bd54d725d70ddefe218d2
kimdongjoon-web <color6763@naver.com>
2024-12-03 22:41:07 +0900

    mod : 작성자 이름으로 필드명 수정

--- src/main/java/com/example/tenpaws/domain/board/dto/response/InquiryResponse.java
+++ src/main/java/com/example/tenpaws/domain/board/dto/response/InquiryResponse.java
@@ -10,7 +10,7 @@ public class InquiryResponse {
     private final Long id;
     private final String title;
     private final String content;
-    private final String userName;
+    private final String writerName;
     private final Long viewCount;
     private final LocalDate created_at;
 
@@ -18,7 +18,7 @@ public class InquiryResponse {
         this.id = inquiry.getId();
         this.title = inquiry.getTitle();
         this.content = inquiry.getContent();
-        this.userName = inquiry.getUser().getUsername();
+        this.writerName = inquiry.getUser() == null ? inquiry.getShelter().getShelterName() : inquiry.getUser().getUsername();
         this.viewCount = inquiry.getViewCount();
         this.created_at = inquiry.getCreated_at();
     }
