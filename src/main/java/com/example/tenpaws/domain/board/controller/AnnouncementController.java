7aa60f1c1d3c2bb62d6ab443ebb20d45f02e1ef7
kimdongjoon-web <color6763@naver.com>
2024-12-03 12:19:12 +0900

    fix : 토큰 정보를 사용하도록 수정

--- src/main/java/com/example/tenpaws/domain/board/controller/AnnouncementController.java
+++ src/main/java/com/example/tenpaws/domain/board/controller/AnnouncementController.java
@@ -1,7 +1,6 @@
 package com.example.tenpaws.domain.board.controller;
 
-import com.example.tenpaws.domain.board.dto.request.CreateAnnouncementRequest;
-import com.example.tenpaws.domain.board.dto.request.UpdateAnnouncementRequest;
+import com.example.tenpaws.domain.board.dto.request.AnnouncementRequest;
 import com.example.tenpaws.domain.board.dto.response.AnnouncementListViewResponse;
 import com.example.tenpaws.domain.board.dto.response.AnnouncementResponse;
 import com.example.tenpaws.domain.board.service.AnnouncementService;
@@ -12,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,8 +22,11 @@ public class AnnouncementController {
 
     @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN')")
     @PostMapping
-    public ResponseEntity<AnnouncementResponse> create(@RequestBody CreateAnnouncementRequest request) {
-        return ResponseEntity.ok(announcementService.create(request));
+    public ResponseEntity<AnnouncementResponse> create(
+            @RequestBody AnnouncementRequest request,
+            Authentication authentication) {
+        String email = authentication.getName();
+        return ResponseEntity.ok(announcementService.create(request, email));
     }
 
     @PreAuthorize("permitAll()")
@@ -43,7 +46,7 @@ public class AnnouncementController {
     @PutMapping("/{announcementId}")
     public ResponseEntity<AnnouncementResponse> update(
             @PathVariable Long announcementId,
-            @RequestBody UpdateAnnouncementRequest request) {
+            @RequestBody AnnouncementRequest request) {
         return ResponseEntity.ok(announcementService.update(announcementId, request));
     }
 
