ee201c2f1529cf7e508b8b3f624411b55f682923
xxxkyeom <seonggyeomgim97@gmail.com>
2024-12-04 09:36:52 +0900

    add: 메서드 추가

--- src/main/java/com/example/tenpaws/domain/user/service/UserService.java
+++ src/main/java/com/example/tenpaws/domain/user/service/UserService.java
@@ -1,11 +1,9 @@
 package com.example.tenpaws.domain.user.service;
 
 import com.example.tenpaws.domain.shelter.dto.ShelterRequestDTO;
-import com.example.tenpaws.domain.user.dto.UserJoinDTO;
-import com.example.tenpaws.domain.user.dto.UserResponseDTO;
-import com.example.tenpaws.domain.user.dto.UserUpdateRequestDTO;
-import com.example.tenpaws.domain.user.dto.UserUpdateResponseDTO;
+import com.example.tenpaws.domain.user.dto.*;
 import com.example.tenpaws.domain.user.entity.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +15,6 @@ public interface UserService {
     UserUpdateResponseDTO updateUser(Long id, UserUpdateRequestDTO userUpdateRequestDTO);
     void deleteUser(Long id);
     List<UserResponseDTO> getAllUsers();
+    List<OAuth2UserDTO> getAllSocialUsers();
+    OAuth2UserDTO getSocialUserInfo(String userId);
 }
