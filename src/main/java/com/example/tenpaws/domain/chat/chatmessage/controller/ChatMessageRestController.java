d2b2e6f200bb933cc8bcbfd81b3a74147c98cdb9
namhyeonh0 <www101365@naver.com>
2024-12-04 14:02:19 +0900

    mod: 채팅 관련 컨트롤러 권한 수정

--- src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatMessageRestController.java
+++ src/main/java/com/example/tenpaws/domain/chat/chatmessage/controller/ChatMessageRestController.java
@@ -21,7 +21,7 @@ public class ChatMessageRestController {
     private final ChatMessageService chatMessageService;
     private final CustomUserDetailsService customUserDetailsService;
 
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_SHELTER', 'ROLE_ADMIN', 'ROLE_SUPER_ADMIN') and @chatRoomServiceImpl.isUserParticipated(#chatRoomId)")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SUPER_ADMIN') or (hasAnyRole('ROLE_USER', 'ROLE_SHELTER') and @chatRoomServiceImpl.isUserParticipated(#chatRoomId))")
     @GetMapping("/{chatRoomId}")
     public ResponseEntity<List<ChatMessageResponse>> getChatMessages(@PathVariable("chatRoomId") Long chatRoomId) {
         List<ChatMessageResponse> chatMessageResponseList = chatMessageService.getChatMessagesByChatRoomId(chatRoomId);
