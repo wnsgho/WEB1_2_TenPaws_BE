db87652bd78dd30ce5095cce2dabef72817defb8
namhyeonh0 <www101365@naver.com>
2024-12-04 12:56:21 +0900

    del: UserController 불필요한 import 삭제

--- src/main/java/com/example/tenpaws/domain/user/controller/UserController.java
+++ src/main/java/com/example/tenpaws/domain/user/controller/UserController.java
@@ -12,7 +12,6 @@ import com.example.tenpaws.global.exception.ErrorCode;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
