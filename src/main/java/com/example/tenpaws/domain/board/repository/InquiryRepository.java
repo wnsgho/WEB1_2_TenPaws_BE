40fb46c3af3298f787d76bfe7eecb7f0d49403ff
kimdongjoon-web <color6763@naver.com>
2024-12-05 17:14:55 +0900

    mod: 작성자이름, 작성자이메일 필드 추가로 인한 변경

--- src/main/java/com/example/tenpaws/domain/board/repository/InquiryRepository.java
+++ src/main/java/com/example/tenpaws/domain/board/repository/InquiryRepository.java
@@ -4,15 +4,7 @@ import com.example.tenpaws.domain.board.entity.Inquiry;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
-    Page<Inquiry> findByUserEmail(String email, Pageable pageable);
-    Page<Inquiry> findByShelterEmail(String email, Pageable pageable);
-
-    @Query("SELECT i FROM Inquiry i WHERE " +
-            "(i.user IS NOT NULL AND i.user.email = :email) OR " +
-            "(i.shelter IS NOT NULL AND i.shelter.email = :email)")
-    Page<Inquiry> findByWriterEmail(@Param("email") String email, Pageable pageable);
+    Page<Inquiry> findByWriterEmail(String email, Pageable pageable);
 }
